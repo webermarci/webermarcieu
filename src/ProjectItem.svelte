6492d5fb492a1327da6a4b27fa73477ea331fb5f
Wéber Marci <webermarton@gmail.com>
2020-12-29 20:52:06 +0100

    Fix links for best practices

--- src/ProjectItem.svelte
+++ src/ProjectItem.svelte
@@ -19,7 +19,9 @@
     <a
       class="flex items-center text-green-500 hover:text-green-300 transition-colors duration-300"
       href={url}
-      target="_blank">
+      target="_blank"
+      rel="noopener"
+      aria-label="Open project">
       <svg
         class="w-6 h-6"
         xmlns="http://www.w3.org/2000/svg"
