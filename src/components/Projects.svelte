e282816b944be67ffd7be0f22cea808523b63773
Wéber Marci <webermarton@gmail.com>
2021-01-18 13:38:37 +0100

    Add projects

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -19,9 +19,25 @@
             url: "https://timato.netlify.app/",
         },
         {
+            title: "go-pipe-parser",
+            description: "A go library to help parse unix pipes.",
+            url: "https://github.com/webermarci/go-pipe-parser",
+        },
+        {
             title: "Portfolio site",
             description: "The source code of this website.",
             url: "https://github.com/webermarci/webermarcieu",
+        },
+        {
+            title: "Svelte.js & Tailwind",
+            description: "My take on the Svelte and Tailwind template.",
+            url: "https://github.com/webermarci/svelte-tailwind-template",
+        },
+        {
+            title: "Svelte.ts & Tailwind",
+            description: "Simliar to the previous one, but with Typescript.",
+            url:
+                "https://github.com/webermarci/svelte-typescript-tailwind-template",
         },
     ];
 </script>
