9c78466c857eb898d517949601090f50f41be618
Wéber Marci <webermarton@gmail.com>
2021-03-16 11:51:14 +0100

    Add postcss defaults

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,10 @@ const preprocess = require('svelte-preprocess')
 
 module.exports = {
     preprocess: preprocess({
+        defaults: {
+            script: "typescript",
+            style: "postcss",
+        },
         postcss: {
             plugins: [
                 require("@tailwindcss/jit"),
