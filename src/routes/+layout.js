6b4f6aff965c67cb132fa9530fa47fc1fc4a8450
webermarci <webermarton@gmail.com>
2023-12-05 11:53:05 +0100

    Change google analytics to posthog

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,15 @@
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
 export const prerender = true;
-export const csr = false;
+
+export const load = async () => {
+    if (browser) {
+        posthog.init(
+            'phc_4XB2LcqTutCvDbtvimDaGsOt04nsZ4f23SdJZbDDdEZ',
+            {
+                api_host: 'https://eu.posthog.com'
+            }
+        )
+    }
+};
